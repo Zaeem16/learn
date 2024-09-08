77d535d819e7dcc643c0755d7e70923cc67c5d53
zaeem <ahmadzaeemzahid@gmail.com>
2024-09-07 20:39:00 -0500

    setting changes

--- javascript/10-css/10f/10f-index.js
+++ javascript/10-css/10f/10f-index.js
@@ -1,17 +1,17 @@
 function changeMode(event) {
-    let button = event.target;
+  let button = event.target;
 
-    if (button.classList.contains("js-button-off")) {
+  if (button.classList.contains("js-button-off")) {
     button.classList.remove("js-button-off");
     button.classList.add("js-button-on");
     console.log("on");
-    }
-    else if (button.classList.contains("js-button-on"))  {
-      button.classList.remove("js-button-on");
-      button.classList.add("js-button-off");
-      console.log("off");
-    }
-    else {
-      console.log("button not found");
-    }
   }
+  else if (button.classList.contains("js-button-on"))  {
+    button.classList.remove("js-button-on");
+    button.classList.add("js-button-off");
+    console.log("off");
+  }
+  else {
+    console.log("button not found");
+  }
+}
