b03aa4e247d0f13576e4bfe218976f3db4ccad94
zaeem <ahmadzaeemzahid@gmail.com>
2024-10-26 10:08:04 -0500

    15e - create isWeekend to check whether the parameter is a weekend day.

--- javascript/15-external-libraries/a-g/script.js
+++ javascript/15-external-libraries/a-g/script.js
@@ -1,3 +1,4 @@
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 const today = dayjs();
 
 /* 15a */
@@ -12,5 +13,22 @@ console.log(after1month.format("MMMM D"));
 const before1month = today.subtract(1, "months");
 console.log(before1month.format("MMMM D"));
 
-/* 15d */ 
-console.log(today.format("dddd"));
+/* 15d */
+const todayDayName = today.format("dddd");
+console.log(todayDayName);
+
+/* 15e */ 
+function isWeekend(date) {
+    const day = dayjs(date).day(); // Get the day of the week (0 = Sunday, 6 = Saturday)
+    if (day === 0) {
+        return "Sunday";
+    }
+    else if (day === 6) {
+        return "Saturday";
+    }
+    else {
+        return "Weekday";
+    }
+}
+
+console.log(isWeekend(today));
