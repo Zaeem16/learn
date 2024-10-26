8f31418f3bfcc0f4204d4d3135404088cd2f2358
zaeem <ahmadzaeemzahid@gmail.com>
2024-10-26 09:25:41 -0500

    15b - display date after 1 month

--- javascript/15-external-libraries/a-g/script.js
+++ javascript/15-external-libraries/a-g/script.js
@@ -1,3 +1,6 @@
 const today = dayjs();
 const after5days = today.add(5, "days");
 console.log(after5days.format("MMMM D"));
+
+const after1month = today.add(1, "months");
+console.log(after1month.format("MMMM D"));
