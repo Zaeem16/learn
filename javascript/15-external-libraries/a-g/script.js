3b2d4b1f1416cc01a4e853eca9f645b58149ec27
zaeem <ahmadzaeemzahid@gmail.com>
2024-10-26 09:29:17 -0500

    15c - display one month before today

--- javascript/15-external-libraries/a-g/script.js
+++ javascript/15-external-libraries/a-g/script.js
@@ -1,6 +1,14 @@
 const today = dayjs();
+
+/* 15a */
 const after5days = today.add(5, "days");
 console.log(after5days.format("MMMM D"));
 
+/* 15b */
 const after1month = today.add(1, "months");
 console.log(after1month.format("MMMM D"));
+
+/* 15c */
+const before1month = today.subtract(1, "months");
+console.log(before1month.format("MMMM D"));
+
